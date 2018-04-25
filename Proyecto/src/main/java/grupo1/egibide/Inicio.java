28e18391127b71235937b1fcc0027b28de6be34c
Unknown <garciadiego769@gmail.com>
2018-04-25 21:01:32 +0200

    Menú principal

--- Proyecto/src/main/java/grupo1/egibide/Inicio.java
+++ Proyecto/src/main/java/grupo1/egibide/Inicio.java
@@ -1,11 +1,35 @@
 package grupo1.egibide;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Inicio {
     private JPanel login;
+    private JLabel titulo;
+    private JButton administrador;
+    private JButton duenyo;
+    private JButton usuario;
+
+    public Inicio() { //Al pulsar en los iconos del menú
+        administrador.addActionListener(new ActionListener() { //ir a pantalla de admin
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        duenyo.addActionListener(new ActionListener() { //ir a pantalla dueño
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        usuario.addActionListener(new ActionListener() { //ir a pantalla usuario
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
